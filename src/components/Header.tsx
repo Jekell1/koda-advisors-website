d07324bab01739bc0ffae0076d8d915008fe19eb
Jeff <jeff@kodaadvisors.com>
2025-08-15 23:28:01 -0400

    Fix logo display using Next.js Image component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import Image from 'next/image'
 import { Menu, X } from 'lucide-react'
 
 const Header = () => {
@@ -18,9 +19,11 @@ const Header = () => {
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <img 
+            <Image 
               src="/husky-logo.png" 
               alt="Koda Advisors" 
+              width={32}
+              height={32}
               className="h-8 w-auto"
             />
           </a>
